b98563dd2444d5732d7c043ab057158a3e061486
Tecu23 <andrei.teculescu@gmail.com>
2023-05-05 13:03:14 +0300

    remove unecessary file

--- nvim/nvim/lua/tecu/core/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-local colorscheme = "rose-pine-moon"
-
-local status_ok, _  = pcall(vim.cmd.colorscheme, colorscheme)
-if not status_ok then
-  return
-end
-
-
--- nvim/nvim/lua/tecu/plugins/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  return
-end
-
-require "tecu.plugins.lsp.mason"
-require("tecu.plugins.lsp.handlers").setup()
-require "tecu.plugins.lsp.null-ls"
