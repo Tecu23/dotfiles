ce2c8919c3472675b5ac5efa4c0cd117291433a6
Tecu23 <andrei.teculescu@gmail.com>
2024-02-23 19:30:58 +0200

    update terminal

---
+++ .config/nvim/lua/tecu/plugins/nvterm.lua
@@ -0,0 +1,53 @@
+local M = {
+  "NvChad/nvterm",
+}
+
+function M.config()
+
+  require("nvterm").setup {
+    terminals = {
+      shell = vim.o.shell,
+      list = {},
+      type_opts = {
+        float = {
+          relative = 'editor',
+          row = 0.3,
+          col = 0.1,
+          width = 0.8,
+          height = 0.3,
+          border = "single",
+        },
+        horizontal = { location = "rightbelow", split_ratio = .3, },
+        vertical = { location = "rightbelow", split_ratio = .5 },
+      }
+    },
+    behavior = {
+      autoclose_on_quit = {
+        enabled = false,
+        confirm = true,
+      },
+      close_on_exit = true,
+      auto_insert = true,
+    },
+  }
+
+  local terminal = require("nvterm.terminal")
+
+  local toggle_modes = {'n', 't'}
+
+  local mappings = {
+    { toggle_modes, '<A-h>', function () terminal.toggle('horizontal') end },
+    { toggle_modes, '<A-v>', function () terminal.toggle('vertical') end },
+    { toggle_modes, '<A-t>', function () terminal.toggle('float') end },
+  }
+
+  local opts = { noremap = true, silent = true }
+
+  for _, mapping in ipairs(mappings) do
+    vim.keymap.set(mapping[1], mapping[2], mapping[3], opts)
+  end
+
+
+end
+
+return M
