48d575978a0874c52621d8c67e0cd2a5d5c53129
Tecu23 <andrei.teculescu@gmail.com>
2024-02-23 21:51:42 +0200

    update telescope to remove .git folder

--- .config/nvim/lua/tecu/plugins/telescope.lua
+++ .config/nvim/lua/tecu/plugins/telescope.lua
@@ -71,7 +71,13 @@ function M.config()
       },
 
       find_files = {
-        hidden = true,
+        find_command = {
+          'rg',
+          '--files',
+          '--hidden',
+          '-g',
+          '!.git'
+        },
         theme = "dropdown",
         previewer = false,
       },
