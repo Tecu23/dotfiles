ce4444462f213feac962ec97fa0172914f0d88f7
Tecu23 <andrei.teculescu@gmail.com>
2024-02-23 21:10:01 +0200

    some changes

--- .config/nvim/lua/tecu/plugins/harpoon.lua
+++ .config/nvim/lua/tecu/plugins/harpoon.lua
@@ -11,8 +11,26 @@ function M.config()
   local keymap = vim.keymap.set
   local opts = { noremap = true, silent = true }
 
-  keymap("n", "<s-m>", "<cmd>lua require('tecu.plugins.harpoon').mark_file()<cr>", opts)
-  keymap("n", "<TAB>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", opts)
+
+  local harpoon = require("harpoon")
+
+  harpoon:setup()
+
+  keymap("n", "<leader>a", function() harpoon:list():append() end)
+  keymap("n", "<s-m>", function() harpoon:list():append() end)
+  keymap("n", "<TAB>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+  
+  keymap("n", "<C-h>", function() harpoon:list():select(1) end)
+  keymap("n", "<C-t>", function() harpoon:list():select(2) end)
+  keymap("n", "<C-n>", function() harpoon:list():select(3) end)
+  keymap("n", "<C-s>", function() harpoon:list():select(4) end)
+
+  -- keymap("n", "<s-m>", "<cmd>lua require('tecu.plugins.harpoon').mark_file()<cr>", opts)
+  -- keymap("n", "<TAB>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", opts)
+
+  -- Toggle previous & next buffers stored within Harpoon list
+  keymap("n", "<C-q>", function() harpoon:list():prev() end)
+  keymap("n", "<C-w>", function() harpoon:list():next() end)
 end
 
 function M.mark_file()
--- .config/nvim/lua/tecu/plugins/lualine.lua
+++ .config/nvim/lua/tecu/plugins/lualine.lua
@@ -1,24 +1,22 @@
 local M = {
   "nvim-lualine/lualine.nvim",
-  dependencies = {
-    "AndreM222/copilot-lualine",
-  },
 }
 
 function M.config()
   require("lualine").setup {
     options = {
-      component_separators = { left = "", right = "" },
-      section_separators = { left = "", right = "" },
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
       ignore_focus = { "NvimTree" },
+      theme = "nord",
     },
     sections = {
-      lualine_a = {},
-      lualine_b = { "branch" },
+      lualine_a = { 'mode' },
+      lualine_b = { "branch", "diff"},
       lualine_c = { "diagnostics" },
-      lualine_x = { "copilot", "filetype" },
+      lualine_x = { "fileformat", "filetype" },
       lualine_y = { "progress" },
-      lualine_z = {},
+      lualine_z = { "location" },
     },
     extensions = { "quickfix", "man", "fugitive" },
   }
