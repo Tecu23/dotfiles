b0ef1aa870c79ecb667a61712f18d5b60ce7baf6
Tecu23 <andrei.teculescu@gmail.com>
2024-02-26 20:42:55 +0200

    add remove to harpoon

--- .config/nvim/lua/tecu/plugins/harpoon.lua
+++ .config/nvim/lua/tecu/plugins/harpoon.lua
@@ -19,14 +19,13 @@ function M.config()
   keymap("n", "<leader>a", function() harpoon:list():append() end)
   keymap("n", "<s-m>", function() harpoon:list():append() end)
   keymap("n", "<TAB>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+  keymap("n", "<s-n>", function() harpoon:list():remove() end)
   
   keymap("n", "<C-h>", function() harpoon:list():select(1) end)
   keymap("n", "<C-t>", function() harpoon:list():select(2) end)
   keymap("n", "<C-n>", function() harpoon:list():select(3) end)
   keymap("n", "<C-s>", function() harpoon:list():select(4) end)
-
-  -- keymap("n", "<s-m>", "<cmd>lua require('tecu.plugins.harpoon').mark_file()<cr>", opts)
-  -- keymap("n", "<TAB>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", opts)
 
   -- Toggle previous & next buffers stored within Harpoon list
   keymap("n", "<C-q>", function() harpoon:list():prev() end)
