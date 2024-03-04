98a7d2ee9f2e07ae0ebbe495f788b1b2838bd276
tecu23 <andrei.teculescu@gmail.com>
2024-03-04 22:50:27 +0200

    add linting for markdown

--- .config/nvim/lua/tecu/lsp/none-ls.lua
+++ .config/nvim/lua/tecu/lsp/none-ls.lua
@@ -1,51 +1,52 @@
 local M = {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim"
-  }
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
 }
 
 function M.config()
-  local null_ls = require "null-ls"
-  local autogroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	local null_ls = require("null-ls")
+	local autogroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-  local formatting = null_ls.builtins.formatting
-  local diagnostics = null_ls.builtins.diagnostics
-  local completions = null_ls.builtins.completion
+	local formatting = null_ls.builtins.formatting
+	local diagnostics = null_ls.builtins.diagnostics
+	local completions = null_ls.builtins.completion
 
-  null_ls.setup {
-    debug = false,
-    sources = {
-        formatting.stylua,
-        formatting.prettier,
-        formatting.black,
-        formatting.gofumpt,
-        formatting.goimports_reviser,
-        formatting.golines,
-        formatting.biome,
+	null_ls.setup({
+		debug = false,
+		sources = {
+			formatting.stylua,
+			formatting.prettier,
+			formatting.black,
+			formatting.gofumpt,
+			formatting.goimports_reviser,
+			formatting.golines,
+			formatting.biome,
+			formatting.remark,
 
-        diagnostics.buf,
-        diagnostics.revive,
+			diagnostics.buf,
+			diagnostics.revive,
+			diagnostics.markdownlint,
 
-        completions.spell,
-    },
-    on_attach = function(client, bufnr)
-      if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({
-          group = autogroup,
-          buffer = bufnr,
-        })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          group = autogroup,
-          buffer = bufnr,
-          callback = function ()
-            vim.lsp.buf.format({ bufnr = bufnr })
-            
-          end
-        })
-      end
-    end,
-  }
+			completions.spell,
+		},
+		on_attach = function(client, bufnr)
+			if client.supports_method("textDocument/formatting") then
+				vim.api.nvim_clear_autocmds({
+					group = autogroup,
+					buffer = bufnr,
+				})
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					group = autogroup,
+					buffer = bufnr,
+					callback = function()
+						vim.lsp.buf.format({ bufnr = bufnr })
+					end,
+				})
+			end
+		end,
+	})
 end
 
 return M
